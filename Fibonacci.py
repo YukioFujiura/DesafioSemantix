9c248aab98034d2a23c8cb2e5c704da680000409
YukioFujiura <33206979+YukioFujiura@users.noreply.github.com>
2017-10-30 00:48:09 -0200

    9 - Calculo da sequencia de fibonacci

---
+++ Fibonacci.py
@@ -0,0 +1,35 @@
+
+# coding: utf-8
+
+# In[32]:
+
+
+def fibonacci_recursivo(n):
+    if n==1 or n==2:
+        return 1
+    else:
+        return fibonacci_recursivo(n-1) + fibonacci_recursivo(n-2)
+
+
+# In[33]:
+
+
+def fibonacci_iterativo(n):
+    a,b = 0, 1
+    for i in range (n):
+        a,b = b,a+b
+    return a
+
+
+# In[60]:
+
+
+def main():
+    n=int(input("Digite um número "))
+    for i in range(1,n+1):
+        print ("Esse eh o fibonacci recursivo é = "+str(fibonacci_recursivo(i)))
+    for i in range(1,n+1):
+        print ("Esse eh o fibonnaci iterativo é = "+ str(fibonacci_iterativo(i)))
+if __name__=="__main__":
+    main()
+
