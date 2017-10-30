d4d47be41cf9fe5e8d625caef07629a3e99fc7b5
YukioFujiura <33206979+YukioFujiura@users.noreply.github.com>
2017-10-30 14:16:53 -0200

    Resposta 2

---
+++ ProcessaArquivo.py
@@ -0,0 +1,23 @@
+
+# coding: utf-8
+
+# In[16]:
+
+
+def processa_arquivo_txt(n):
+    print("Processando txt")
+def processa_arquivo_csv(n):
+    print("Processando csv")
+
+def main():
+    arquivo = input("Digite o nome do arquivo no fomarto 'nome_do_arquivo.extensao' : ")
+    extensao = arquivo[-3:]
+    if extensao == ("txt"):
+        processa_arquivo_txt(arquivo)
+    elif extensao == ("csv"):
+        processa_arquivo_csv(arquivo)
+    else:
+        print("O arquivo não se encontra na extensão .txt ou .csv")
+if __name__ == "__main__":
+    main()
+
